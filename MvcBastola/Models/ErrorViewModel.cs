0a1ff93a7851b7da07b0fb2637b65bf7f5f30d84
Bibek Bastola <bibekbastola@Bibeks-MacBook-Air.local>
2022-08-13 17:56:31 -0500

    initial commit

---
+++ MvcBastola/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcBastola.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
